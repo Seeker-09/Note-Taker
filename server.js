1fba9b8fe17b506348fbd4b8435b3fd1eaf68012
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-06-28 19:38:47 -0400

    refatored the READ api route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {notes} = require("./db/db.json");
+const notes = require("./db/db.json");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+const { fstat } = require("fs");
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
@@ -13,6 +15,13 @@ app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+    })
 })
 
 app.get("*", (req, res) => {
