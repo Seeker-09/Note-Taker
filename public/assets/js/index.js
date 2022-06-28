2b35f25c3c025544a4dc05e43dd2c2d5848a9423
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-06-28 18:29:00 -0400

    can post data to db.json

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -39,7 +39,7 @@ const saveNote = (note) =>
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(note),
+    body: JSON.stringify(note)
   });
 
 const deleteNote = (id) =>
